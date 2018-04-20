785587d38a5c6e02c3c5a9354decba16151c1997
ianagbip1oti <ianagbip1oti@gmail.com>
2018-04-20 06:26:06 +0000

    Remove Resource suffix

--- src/codegen/ruby/java_types.rb
+++ src/codegen/ruby/java_types.rb
@@ -1,17 +1,17 @@
 # Using non-primitive types, as we may want to wrap in Optional
 
 TYPES = {
-  'array of DM channel objects' => 'ImmutableList<ChannelResource>',
-  'array of overwrite objects' => 'ImmutableList<OverwriteResource>',
+  'array of DM channel objects' => 'ImmutableList<Channel>',
+  'array of overwrite objects' => 'ImmutableList<Overwrite>',
   'array of strings' => 'ImmutableList<String>',
-  'array of Unavailable Guild objects' => 'ImmutableList<UnavailableGuildResource>',
-  'array of user objects' => 'ImmutableList<UserResource>',
+  'array of Unavailable Guild objects' => 'ImmutableList<UnavailableGuild>',
+  'array of user objects' => 'ImmutableList<User>',
   'bool' => 'Boolean',
   'integer' => 'Long',
   'ISO8601 timestamp' => 'java.time.OffsetDateTime',
   'snowflake' => 'Snowflake',
   'string' => 'String',
-  'user object' => 'UserResource'
+  'user object' => 'User'
 }
 
 def get_java_type(t)
