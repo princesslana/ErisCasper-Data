b5487581fe83a399d16a3f741a07563326a28f89
ianagbip1oti <ianagbip1oti@gmail.com>
2018-05-06 23:25:34 +0000

    Add Guild Create/Update/Delete

--- src/codegen/ruby/event_types.rb
+++ src/codegen/ruby/event_types.rb
@@ -6,5 +6,8 @@ EVENTS = {
   channel_update: 'Channel',
   channel_delete: 'Channel',
   channel_pins_update: 'ChannelPinsUpdateEventData',
+  guild_create: 'Guild',
+  guild_update: 'Guild',
+  guild_delete: 'UnavailableGuild',
   typing_start: 'TypingStartEventData'
 }
--- src/codegen/ruby/java_types.rb
+++ src/codegen/ruby/java_types.rb
@@ -1,16 +1,31 @@
 # Using non-primitive types, as we may want to wrap in Optional
 
 TYPES = {
+  'activity object' => 'Activity',
+  'array of channel objects' => 'ImmutableList<Channel>',
   'array of DM channel objects' => 'ImmutableList<Channel>',
+  'array of emoji objects' => 'ImmutableList<Emoji>',
+  'array of guild member objects' => 'ImmutableList<GuildMember>',
   'array of overwrite objects' => 'ImmutableList<Overwrite>',
+  'array of partial presence update objects' => 'ImmutableList<PresenceUpdate>',
+  'array of partial voice state objects' => 'ImmutableList<VoiceState>',
+  'array of role objects' => 'ImmutableList<Role>',
+  'array of role object ids' => 'ImmutableList<Snowflake>',
+  'array of snowflakes' => 'ImmutableList<Snowflake>',
   'array of strings' => 'ImmutableList<String>',
+  'array of two integers (current_size, max_size)' => 'ImmutableList<Long>',
   'array of Unavailable Guild objects' => 'ImmutableList<UnavailableGuild>',
   'array of user objects' => 'ImmutableList<User>',
+  'assets object' => 'ActivityAssets',
   'bool' => 'Boolean',
+  'int' => 'Long',
   'integer' => 'Long',
   'ISO8601 timestamp' => 'java.time.OffsetDateTime',
+  'object' => 'User',
+  'party object' => 'ActivityParty',
   'snowflake' => 'Snowflake',
   'string' => 'String',
+  'timestamps object' => 'ActivityTimestamps',
   'user object' => 'User'
 }
 
