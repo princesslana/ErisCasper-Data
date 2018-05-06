0baca765b1d9324ab4a0aa6a40fcbba1f8b41106
ianagbip1oti <ianagbip1oti@gmail.com>
2018-05-06 22:50:03 +0000

    Add channel pins update

--- src/codegen/ruby/event_types.rb
+++ src/codegen/ruby/event_types.rb
@@ -5,5 +5,6 @@ EVENTS = {
   channel_create: 'Channel',
   channel_update: 'Channel',
   channel_delete: 'Channel',
+  channel_pins_update: 'ChannelPinsUpdateEventData',
   typing_start: 'TypingStartEventData'
 }
