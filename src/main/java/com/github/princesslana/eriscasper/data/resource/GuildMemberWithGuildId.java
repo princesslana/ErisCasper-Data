766699df6982fba95fb48900c47246864c93f9b8
CoreyShupe <coreyshupe101@gmail.com>
2018-06-03 10:35:08 -0400

    Removed unused stuff

--- src/main/java/com/github/princesslana/eriscasper/data/resource/GuildMemberWithGuildId.java
+++ src/main/java/com/github/princesslana/eriscasper/data/resource/GuildMemberWithGuildId.java
@@ -2,21 +2,36 @@ package com.github.princesslana.eriscasper.data.resource;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.github.princesslana.eriscasper.data.Data;
 import com.github.princesslana.eriscasper.data.DataException;
 import com.github.princesslana.eriscasper.data.Snowflake;
 
-public class GuildMemberWithGuildId extends WithGuildId<GuildMember> {
+public class GuildMemberWithGuildId {
 
-  public GuildMemberWithGuildId(Snowflake guildId, GuildMember member) {
-    super(guildId, member);
-  }
+  private Snowflake guildId;
+  private GuildMember member;
+
+  private GuildMemberWithGuildId() {}
 
   @JsonCreator
   public GuildMemberWithGuildId(JsonNode json) throws DataException {
-    super(GuildMember.class, json);
+    guildId = Snowflake.of(json.get("guild_id").asText());
+    member = Data.fromJson(json, GuildMember.class);
   }
 
+  public Snowflake getGuildId() {
+    return guildId;
+  }
+
+  @SuppressWarnings("unused")
   public GuildMember getGuildMember() {
-    return getValue();
+    return member;
+  }
+
+  public static GuildMemberWithGuildId from(Snowflake guildId, GuildMember member) {
+    GuildMemberWithGuildId guildMember = new GuildMemberWithGuildId();
+    guildMember.guildId = guildId;
+    guildMember.member = member;
+    return guildMember;
   }
 }
