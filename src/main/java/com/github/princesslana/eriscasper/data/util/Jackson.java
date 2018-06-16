0789df732e5dc73750fc08b560f97a1ccefd4195
ianagbip1oti <ianagbip1oti@gmail.com>
2018-06-16 19:48:17 +0000

    Add Data.toJsonString. Filter Optional/nulls as needed

--- src/main/java/com/github/princesslana/eriscasper/data/util/Jackson.java
+++ src/main/java/com/github/princesslana/eriscasper/data/util/Jackson.java
@@ -1,22 +1,35 @@
 package com.github.princesslana.eriscasper.data.util;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.util.Optional;
 
 public class Jackson {
   private Jackson() {}
 
   public static ObjectMapper newObjectMapper() {
     ObjectMapper jackson = new ObjectMapper();
+    jackson.setDefaultPropertyInclusion(
+        JsonInclude.Value.construct(JsonInclude.Include.ALWAYS, JsonInclude.Include.ALWAYS)
+            .withValueFilter(NotEmptyFilter.class)
+            .withContentFilter(NotEmptyFilter.class));
     jackson.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     jackson.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     jackson.registerModule(new GuavaModule());
     jackson.registerModule(new Jdk8Module());
     jackson.registerModule(new JavaTimeModule());
     return jackson;
+  }
+
+  private static class NotEmptyFilter {
+    @Override
+    public boolean equals(Object rhs) {
+      return Optional.empty().equals(rhs);
+    }
   }
 }
