1a66f6c5541960385f22b7d564c609706d0b5628
ianagbip1oti <ianagbip1oti@gmail.com>
2018-06-19 02:16:03 +0000

    Add types for array of two integers usage

--- src/test/java/com/github/princesslana/eriscasper/data/gateway/TestIdentifyPayload.java
+++ src/test/java/com/github/princesslana/eriscasper/data/gateway/TestIdentifyPayload.java
@@ -3,6 +3,7 @@ package com.github.princesslana.eriscasper.data.gateway;
 import com.github.princesslana.eriscasper.data.Data;
 import com.github.princesslana.eriscasper.data.DataException;
 import java.util.Map;
+import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.Test;
 
@@ -10,20 +11,35 @@ public class TestIdentifyPayload {
 
   @Test
   public void toJsonString_whenAbsentFields_shouldNotIncludeAbsentFields() throws DataException {
-    ConnectionPropertiesPayload connectionProperties =
-        ImmutableConnectionPropertiesPayload.builder()
-            .os(System.getProperty("os.name"))
-            .browser("ErisCasper-Data")
-            .device("ErisCasper-Data")
-            .build();
-
     IdentifyPayload payload =
-        ImmutableIdentifyPayload.builder().token("abc").properties(connectionProperties).build();
+        ImmutableIdentifyPayload.builder().token("abc").properties(connectionProperties()).build();
 
     Assertions.assertThat(Data.fromJson(Data.toJsonString(payload), Map.class))
         .hasSize(2)
         .containsEntry("token", "abc")
         .containsKey("properties")
         .doesNotContainKeys("compress", "large_threshold", "shard", "presence");
+  }
+
+  @Test
+  public void toJsonString_whenSharding_shouldIncludeShardingAsArray() throws DataException {
+    IdentifyPayload payload =
+        ImmutableIdentifyPayload.builder()
+            .token("abc")
+            .properties(connectionProperties())
+            .shard(ShardPayload.of(2, 3))
+            .build();
+
+    Assertions.assertThat(Data.fromJson(Data.toJsonString(payload), IdentifyPayload.class))
+        .hasFieldOrPropertyWithValue("token", "abc")
+        .hasFieldOrPropertyWithValue("shard", Optional.of(ShardPayload.of(2, 3)));
+  }
+
+  private static ConnectionPropertiesPayload connectionProperties() {
+    return ImmutableConnectionPropertiesPayload.builder()
+        .os(System.getProperty("os.name"))
+        .browser("ErisCasper-Data")
+        .device("ErisCasper-Data")
+        .build();
   }
 }
