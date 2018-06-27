a4baca1f14810f693eac513b760d93472092201a
Corey Shupe <coreyshupe101@gmail.com>
2018-06-26 20:48:39 -0400

    Spotless

--- src/test/java/com/github/princesslana/eriscasper/data/TestData.java
+++ src/test/java/com/github/princesslana/eriscasper/data/TestData.java
@@ -75,7 +75,8 @@ public class TestData {
 
   @Test
   public void toQueryString_whenPopulated_shouldFormat() {
-    Assertions.assertThat(Data.toQueryString(Obj.of(Optional.of("test")))).isEqualTo("field=\"test\"");
+    Assertions.assertThat(Data.toQueryString(Obj.of(Optional.of("test"))))
+        .isEqualTo("field=\"test\"");
   }
 
   @Test
